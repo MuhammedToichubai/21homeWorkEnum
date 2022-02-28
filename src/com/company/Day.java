9a20161fb5427fae94184d0e80173e2c9bfc02c7
Muhammed <muhammedtoichubai@gmail.com>
2022-02-27 23:15:48 -0600

    Initial commit

---
+++ src/com/company/Day.java
@@ -0,0 +1,21 @@
+package com.company;
+//monday tuesday wednesday thursday saturday sunday
+public enum Day {
+
+    ДУЙШОМБУ,
+    ШЕЙШЕМБИ,
+    ШАРШЕМБИ,
+    БЕЙШЕМБИ,
+    ЖУМА,
+    ИШЕМБИ,
+    ЖЕКШЕМБИ,
+    ;
+
+    Day() {
+    }
+
+
+
+
+
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,46 @@
+package com.company;
+
+import java.util.Locale;
+import java.util.Objects;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        Кундорго enum тузунуз (Monday, Tuesday ...)
+//        Бир кунду белгилениз консолдон.
+//        Эгер Monday болсо - анда "Дуйшомбу куну жава окуйм" же
+//        Tuesday болсо - анда "Шейшемби куну англис тили сабагын окуйм"
+//        деп кунго жараша кайсы сабакты окуганынызды консольго чыгарыныз.
+//        Жуманын кундорун кыргыз тилинде корсотунуз.
+        System.out.println("Куш келдинниз Мухаммеддин жазган кодуна.\nКонсолдон бир кунду жазыны,(мисалы:'жума')же болбосо башка кунду.");
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+
+        Day day = null;
+        try {
+            day = Day.valueOf(str.toUpperCase(Locale.ROOT));
+        } catch (IllegalArgumentException e) {
+            System.out.println("YOU WRITE WRONG!//СИЗ КАТА ЖАЗЫП ЖАТАСЫЗ!");
+        }
+
+        if (day != null) {
+            switch (day) {
+                case ДУЙШОМБУ -> System.out.println("18:00 | Theory lesson | Space x.");
+                case ШЕЙШЕМБИ -> System.out.println("15:30 | English lesson | Google.");
+                case ШАРШЕМБИ -> System.out.println("18:00 | Theory lesson | Facebook.");
+                case БЕЙШЕМБИ -> System.out.println("11:00 | Practice lesson | Facebook\n15:30 Soft skills lesson | Space x.");
+                case ЖУМА -> System.out.println("18:00 | Theory lesson | Space x. ");
+                case ИШЕМБИ -> System.out.println("No lessons for this day.");
+                case ЖЕКШЕМБИ -> System.out.println("Rest please, sunday is a day off.");
+            }
+            System.out.println(day);
+        }else {
+            System.out.println("NO SUCH A DAY!//МЫНДАЙ КҮН ЖОК!");
+        }
+
+
+
+
+    }
+}
